71ddeed95557ef1f2ff295d3113c4a366ada2519
mikk369 <mikk.ereline@vikk.ee>
2022-06-30 00:08:44 +0300

    output cards html on overview.html

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-const express = require('express');
-const app = express();
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+// const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-const port = 3000;
-app.listen(port),
-  () => {
-    console.log(`App running on port ${port}...`);
-  };
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+// const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+// console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
